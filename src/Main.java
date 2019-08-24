b625865e045a90e500b7848d56a70a3490cf6b48
Anna Hetaliuk <aprymakova@mail.ru>
2019-08-24 13:49:10 +0200

    A loop is created

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Fight!!!!");
         System.out.println("Stop!!!");
         System.out.println("Kill!!!");
+        for (int i=0; i>30;i++){
+            System.out.println(i);
+        }
     }
 }
